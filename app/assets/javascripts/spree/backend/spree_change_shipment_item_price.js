d44622f1eba189fd7323cdf966e31092e927d33f
mike <mstate@gmail.com>
2015-02-16 23:13:25 -0500

    can't override js

--- app/assets/javascripts/spree/backend/spree_change_shipment_item_price.js
+++ app/assets/javascripts/spree/backend/spree_change_shipment_item_price.js
@@ -1,4 +1,6 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 //= require spree/backend
+//= require backend/line_items
+
 //= require_tree .
