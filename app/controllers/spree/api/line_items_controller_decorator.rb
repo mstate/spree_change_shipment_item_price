2800664db206281b91ef14c56b1b711a53f2efe0
mike <mstate@gmail.com>
2015-02-16 23:55:51 -0500

    Force new line item one "creation" if have spree_option_is_product installed.

--- app/controllers/spree/api/line_items_controller_decorator.rb
+++ app/controllers/spree/api/line_items_controller_decorator.rb
@@ -1,4 +1,20 @@
 Spree::Api::LineItemsController.class_eval do
+
+	def method_name
+		options[:force_new_line_item]
+	end
+	def create
+    variant = Spree::Variant.find(params[:line_item][:variant_id])
+    # added force_new_line_item: true - requires spree_option_is_product to ensure new line item created.
+    @line_item = order.contents.add(variant, (params[:line_item][:quantity] || 1), force_new_line_item: true)
+
+    if @line_item.errors.empty?
+      respond_with(@line_item, status: 201, default_template: :show)
+    else
+      invalid_resource!(@line_item)
+    end
+  end
+
 	private	
 	  def line_items_attributes
 	    attributes_to_return = {line_items_attributes: {
